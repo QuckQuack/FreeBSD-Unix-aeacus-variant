c950ab97f0277582789ebbd0e36ada48e9082436
Hastings <brody.a.hastings@gmail.com>
2026-08-07 16:59:40 -0500

    Delete gui_linux.go

--- gui_linux.go
+++
@@ -1,8 +0,0 @@
-package main
-
-func launchIDPrompt() {
-	err := shellCommand(`zenity --title "Team ID Prompt" --text "Enter your Team ID below!" --entry > ` + dirPath + "TeamID.txt")
-	if err != nil {
-		fail("Error running ID prompt command: " + err.Error())
-	}
-}

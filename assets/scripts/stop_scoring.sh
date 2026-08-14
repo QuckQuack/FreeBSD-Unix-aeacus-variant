cfd495c3de57c0bdd5da469a5e3e7ffd24518976
acamq <179265037+acamq@users.noreply.github.com>
2026-08-14 17:34:35 -0600

    Fix FreeBSD Stop Scoring launcher

--- assets/scripts/stop_scoring.sh
+++ assets/scripts/stop_scoring.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env sh
 
-if zenity --question \
+if /usr/local/bin/zenity --question \
 	--text="Would you like to stop scoring for this image?" \
 	--title="Aeacus SE"; then
-	notify-send -i /opt/aeacus/assets/img/logo.png "Aeacus SE" "Stopping scoring, and shutting down."
-	service CSSClient stop
-	pkill -9 phocus
-	rm -f /opt/aeacus/phocus /opt/aeacus/scoring.dat
-	shutdown now
+	/usr/local/bin/notify-send -i /opt/aeacus/assets/img/logo.png "Aeacus SE" "Stopping scoring, and shutting down."
+	if ! /usr/sbin/service aeacus stop; then
+		/usr/local/bin/notify-send -i /opt/aeacus/assets/img/logo.png "Aeacus SE" "Unable to stop the aeacus service."
+		exit 1
+	fi
+	/bin/rm -f /opt/aeacus/phocus /opt/aeacus/scoring.dat
+	/sbin/shutdown -p now
 else
-	notify-send -i /opt/aeacus/assets/img/logo.png "Aeacus SE" "Confirmation failed!"
+	/usr/local/bin/notify-send -i /opt/aeacus/assets/img/logo.png "Aeacus SE" "Confirmation failed!"
 fi

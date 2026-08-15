7e12671f47bdb6f03b4e29eefe923993dcae5bc1
acamq <179265037+acamq@users.noreply.github.com>
2026-08-14 21:07:29 -0600

    Make Stop Scoring shutdown idempotent

--- assets/scripts/stop_scoring.sh
+++ assets/scripts/stop_scoring.sh
@@ -4,7 +4,7 @@ if /usr/local/bin/zenity --question \
 	--text="Would you like to stop scoring for this image?" \
 	--title="Aeacus SE"; then
 	/usr/local/bin/notify-send -i /opt/aeacus/assets/img/logo.png "Aeacus SE" "Stopping scoring, and shutting down."
-	if ! /usr/sbin/service aeacus stop; then
+	if ! /usr/sbin/service aeacus stop && /usr/sbin/service aeacus onestatus >/dev/null 2>&1; then
 		/usr/local/bin/notify-send -i /opt/aeacus/assets/img/logo.png "Aeacus SE" "Unable to stop the aeacus service."
 		exit 1
 	fi
